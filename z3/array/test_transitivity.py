12689dd5338e0622807ce950ccba4dd46ecaad44
Mike Solomon <mike@meeshkan.com>
2019-07-29 22:58:36 +0300

    Adds more transitive tests

--- z3/array/test_transitivity.py
+++ z3/array/test_transitivity.py
@@ -1,4 +1,5 @@
 from z3 import *
+import pytest
 
 def test_transitive():
   binop = ArraySort(IntSort(), ArraySort(IntSort(), IntSort()))
@@ -8,9 +9,65 @@ def test_transitive():
   s.add(a == Lambda(x, Lambda(y, x + y)))
   s.add(b == Lambda(w, Lambda(z, a[z][w])))
   s.push()
-  s.add(a==b)
+  s.add(a == b)
   assert s.check() == sat
   s.pop()
   s.push()
-  s.add(a!=b)
+  s.add(a != b)
   assert s.check() == unsat
+
+@pytest.mark.skip
+def test_not_transitive():
+  binop = ArraySort(IntSort(), ArraySort(IntSort(), IntSort()))
+  a, b = Consts('a b', binop)
+  w, x, y, z = Ints('w x y z')
+  s = Solver()
+  s.add(a == Lambda(x, Lambda(y, x - y)))
+  s.add(b == Lambda(w, Lambda(z, a[z][w])))
+  #below is just as bad
+  #s.add(ForAll([w,z], b[w][z] == a[z][w]))
+  s.push()
+  s.add(a != b)
+  assert s.check() == sat
+  s.pop()
+  s.push()
+  s.add(a == b)
+  assert s.check() == unsat
+
+def test_not_transitive2():
+  a = Function('a', IntSort(), IntSort(), IntSort())
+  b = Function('b', IntSort(), IntSort(), IntSort())
+  w, x, y, z = Ints('w x y z')
+  s = Solver()
+  s.add(ForAll([x,y], a(x,y) == x - y))
+  s.add(ForAll([w,z], b(w,z) == a(z, w)))
+  s.push()
+  s.add(a != b)
+  assert s.check() == sat
+  s.pop()
+  s.push()
+  s.add(a == b)
+  assert s.check() == unsat
+
+def test_not_transitive_with_existential_qualifier():
+  binop = ArraySort(IntSort(), ArraySort(IntSort(), IntSort()))
+  a, b = Consts('a b', binop)
+  w, x, y, z = Ints('w x y z')
+  s = Solver()
+  s.add(a == Lambda(x, Lambda(y, x - y)))
+  s.add(b == Lambda(w, Lambda(z, a[z][w])))
+  s.push()
+  s.add(Exists([x,y], a[x][y] != b[x][y]))
+  assert s.check() == sat
+
+def test_transitive_with_existential_qualifier():
+  binop = ArraySort(IntSort(), ArraySort(IntSort(), IntSort()))
+  a, b = Consts('a b', binop)
+  w, x, y, z = Ints('w x y z')
+  s = Solver()
+  s.add(a == Lambda(x, Lambda(y, x + y)))
+  s.add(b == Lambda(w, Lambda(z, a[z][w])))
+  s.push()
+  s.add(Exists([x,y], a[x][y] != b[x][y]))
+  assert s.check() == unsat
+  
