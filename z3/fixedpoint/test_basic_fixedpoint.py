06f1c8cdd490f636ced4743fd161b1aee4c1a01e
Mike Solomon <mike@meeshkan.com>
2019-08-04 10:47:54 +0300

    Builds up theory of lists

--- z3/fixedpoint/test_basic_fixedpoint.py
+++ z3/fixedpoint/test_basic_fixedpoint.py
@@ -113,6 +113,68 @@ def test_mseq_hangs():
   assert fp.query(mc(5,120)) == sat
   assert fp.query(mc(5,24)) == unsat
 
+#z3.z3types.Z3Exception: b'recursive predicate (= (:var 3) (cdr (:var 0))) occurs nested in the body of a rule'
+@pytest.mark.skip
+def test_ll_len_with_cdr():
+  ll = Datatype('ll')
+  ll.declare('end')
+  ll.declare('cons', ('car', IntSort()), ('cdr', ll))
+  ll = ll.create()
+  length = Function('length', ll, IntSort(), BoolSort())
+  add = Function('add', IntSort(), IntSort(), IntSort(), BoolSort())
+  x,y,z = Ints('x y z')
+  c, d, e = Consts('c d e', ll)
+  lc = Const('lc', ArraySort(ll,ll))
+
+  fp = Fixedpoint()
+  fp.declare_var(x,y,z,c,d)
+  fp.register_relation(length, add, ll.cdr)
+  fp.fact(add(x,y,x+y))
+  fp.rule(length(c, 0), ll.is_end(c))
+  fp.rule(length(c, x), [ll.is_cons(c), add(1, z, x), length(ll.cdr(c), z)])
+  assert fp.query(length(ll.end, 0)) == sat
+  assert fp.query(length(ll.cons(0, ll.end), 1)) == sat
+
+def test_ll_sum_custom_function():
+  list_sum = Function('list_sum', IntSort(), IntSort(), BoolSort())
+  set_sum = Function('set_sum', IntSort(), IntSort(), BoolSort())
+  cons = Function('cons', IntSort(), IntSort(), IntSort(), BoolSort())
+  car = Function('car', IntSort(), IntSort(), BoolSort())
+  has = Function('has', IntSort(), IntSort(), BoolSort())
+  add = Function('add', IntSort(), IntSort(), IntSort(), BoolSort())
+  a,b,c,d,e,f,g,h,i = Ints('a b c d e f g h i')
+  x,y,z = Bools('x y z')
+  
+  fp = Fixedpoint()
+  fp.declare_var(a,b,c,d,e,f,g,h,i,x,y,z)
+  fp.register_relation(list_sum, set_sum, cons, add, car, has)
+  fp.fact(add(a, b, a + b))
+  fp.rule(list_sum(a, 0), a <= 0)
+  fp.rule(car(a, b), [a > 0, cons(b, a - 1, a)])
+  fp.rule(list_sum(a, i), [a > 0, cons(b, a - 1, a), list_sum(a - 1, c), add(b, c, i)])
+  #fp.rule(set_sum(a, i), [And(a > 0, Not(has(a-1, i))), cons(b, a - 1, a), set_sum(a - 1, c), add(b, c, i)])
+  #fp.rule(set_sum(a, i), [And(a > 0, has(a-1, i)), set_sum(a - 1, i)])
+  fp.rule(has(a, b), [a > 0, cons(c, a - 1, a), Or(has(a - 1, b), c == b)])
+  assert fp.query(list_sum(0, 0)) == sat
+  fp.fact(cons(5, 0, 1))
+  fp.fact(cons(6, 1, 2))
+  fp.fact(cons(5, 2, 3))
+  fp.fact(cons(6, 3, 4))
+  assert fp.query(list_sum(2,12)) == unsat
+  assert fp.query(list_sum(2,11)) == sat
+  assert fp.query(list_sum(4,22)) == sat
+  #assert fp.query(set_sum(4,22)) == unsat
+  #assert fp.query(set_sum(4,11)) == sat 
+  assert fp.query(car(1, 5)) == sat
+  assert fp.query(car(1, 6)) == unsat
+  assert fp.query(has(2, 6)) == sat
+  assert fp.query(has(2, 5)) == sat
+  assert fp.query(has(2, 4)) == unsat
+  assert fp.query(has(0, 4)) == unsat
+  assert fp.query(has(0, 5)) == unsat
+  assert fp.query(has(1, 5)) == sat
+
+@pytest.mark.skip
 def test_aoc():
   one_less = Function('one_less', SetSort(IntSort()), SetSort(IntSort()), BoolSort())
   length = Function('one_less', SetSort(IntSort()), IntSort(), BoolSort())
@@ -131,8 +193,8 @@ def test_aoc():
   fp.rule(one_less(a, b), # one_less doesn't represent a function as anything can be chosen...
           [
             ForAll(x, Implies(Not(a[x]), Not(b[x]))),
-            Exists(x, And(a[x], Not(b[x]))),
-            Exists(x, ForAll(y, If(x==y, True, b[y]) == a[y]))])
+            Implies(Exists(x, a[x]), Exists(x, And(a[x], Not(b[x])))),
+            Implies(Exists(x, a[x]), Exists(x, ForAll(y, If(x==y, True, b[y]) == a[y])))])
   fp.rule(length(a, 0), ForAll(x, Not(a[x])))
   fp.rule(length(a, x), [Exists(y, a[y]), add(1, y, x), length(b, y), one_less(a, b)])
 
@@ -143,11 +205,11 @@ def test_aoc():
   assert fp.query(length(Lambda(x, And(x >= 0, x < 2)), 2)) == sat # struggles with anything over 2
   #assert fp.query(length(Store(es, 0, True), 0)) == unsat # struggles with unsat
 
+
 # does not work
 # stuff like a[x] looks fishy...
-# has(a, x, a[x]) also doesn't work
-# because it introduces two unknowns that are not resolved in the body of the horn clause
-# in max_s(a, x), x can only be resolved at the very end, can't be otherwise used when reasoning about a
+# basically, anything that accesses an array with a quantifier or otherwise seems to break
+# in this case, max_s is the culprit...
 @pytest.mark.skip
 def test_aoc_max():
   rme = Function('rme', SetSort(IntSort()), IntSort(), SetSort(IntSort()), BoolSort())
